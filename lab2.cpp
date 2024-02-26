3cd34703e275f919c6ccb1ce3fcedeb093bcc30d
Alexandr Varnakin <145104896+AlexandrNerf@users.noreply.github.com>
2024-02-26 20:52:09 +0700

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -20,14 +20,12 @@ double cpuSecond()
 
 void simple_iteration_method_1(double* a, double* b, double* x, int thr)
 {
-    double sum = 0.0, error = 0.0;
-    double err_nm = 0.0, err_dnm = 0.0;
-
+    double sum = 0.0, error = 0.0, err = 0.0, err_dnm = 0.0;
     int i, j, k;
     for (k = 0; k < MAX_ITER; k++)
 	{
 		error = 0.0;
-		#pragma omp parallel for private(i, j, sum, err_nm, err_dnm) shared(a, b, x, error) num_threads(thr)
+		#pragma omp parallel for private(i, j, sum, err, err_dnm) shared(a, b, x, error) num_threads(thr)
         for (i = 0; i < N; i++)
 		{
             sum = 0.0;
@@ -35,28 +33,26 @@ void simple_iteration_method_1(double* a, double* b, double* x, int thr)
 			{
                 sum += a[i*N+j] * x[j];
             }
-            err_nm += (sum - b[i]) * (sum - b[i]);
+            err += (sum - b[i]) * (sum - b[i]);
             err_dnm += b[i] * b[i];
 			x[i] = (x[i] - 0.01*(sum-b[i]));
         }
 
-        error = err_nm / err_dnm;
+        error = err / err_dnm;
 
         if (error < EPSILON*EPSILON)
 		{
             break;
         }   
     }
-    // cout << x[0] << ' ' << x[1] << endl;
 }
 
 void simple_iteration_method_2(double* a, double* b, double* x, int thr)
 {
-    double sum = 0.0, error = 0.0;
-    double err_nm = 0.0, err_dnm = 0.0;
+    double sum = 0.0, error = 0.0, err = 0.0, err_dnm = 0.0;
 
     int i, j, k;
-    #pragma omp parallel shared(a, b, x, error) private(i, j, sum, err_nm, err_dnm) num_threads(thr)
+    #pragma omp parallel shared(a, b, x, error) private(i, j, sum, err, err_dnm) num_threads(thr)
     {
         for (k = 0; k < MAX_ITER; k++)
         {
@@ -68,12 +64,12 @@ void simple_iteration_method_2(double* a, double* b, double* x, int thr)
                 {
                     sum += a[i*N+j] * x[j];
                 }
-                err_nm += (sum - b[i]) * (sum - b[i]);
+                err += (sum - b[i]) * (sum - b[i]);
                 err_dnm += b[i] * b[i];
                 x[i] = (x[i] - 0.01*(sum-b[i]));
             }
 
-            error = err_nm / err_dnm;
+            error = err / err_dnm;
 
             if (error < EPSILON*EPSILON)
             {
@@ -81,7 +77,6 @@ void simple_iteration_method_2(double* a, double* b, double* x, int thr)
             }   
         }
     }
-    // cout << x[0] << ' ' << x[1] << endl;
 }
 
 
@@ -89,55 +84,50 @@ int main()
 {
     ofstream file("result.txt");
     
-    double* A = new double[N * N]; // Матрица коэффициентов
-    double* B = new double[N];     // Вектор правой части
-    double* x = new double[N];     // Вектор неизвестных
+    double* A = new double[N * N];
+    double* B = new double[N];    
+    double* x = new double[N];    
 
     int num_procs = omp_get_num_procs(); // Получаем количество доступных ядер
     int max_threads = num_procs > 1 ? num_procs : 1;
 
-    
-    for (int w = 1; w <= 2; w++)
+    for (i = 0; i < N; i++)
     {
-        cout << w << endl;
-        file << w << endl;
-        for (int thr = 1; thr <= max_threads; thr++)
+	for (j = 0; j < N; j++)
+		{
+	    if (i == j)
+		A[i * N + j] = 2.0;
+	    else
+		A[i * N + j] = 1.0;
+	}
+    }
+    for (int th = 1; th <= 2; w++)
+    {
+        file << th << endl;
+        for (int THREAD = 1; THREAD <= max_threads; thr++)
         {
             int i, j;
-            for (i = 0; i < N; i++)
-            {
-                for (j = 0; j < N; j++)
-		        {
-                    if (i == j)
-                        A[i * N + j] = 2.0;
-                    else
-                        A[i * N + j] = 1.0;
-                }
-            }
-
             for (j = 0; j < N; j++)
-	        {
-            B[j] = N+1;
-            x[j] = 0.0;
+	    {
+	        B[j] = N+1;
+	        x[j] = 0.0;
             } 
         
             double tm = cpuSecond();
-            if (w == 1)
+            if (th == 1)
             {
-                simple_iteration_method_1(A, B, x, thr);
+                simple_iteration_method_1(A, B, x, THREAD);
             }
             else
             {
-                simple_iteration_method_2(A, B, x, thr);
+                simple_iteration_method_2(A, B, x, THREAD);
             }
             tm = cpuSecond() - tm;
-            cout << "For " << thr << " threads: " << fixed << setprecision(6) << tm << endl;
             file << fixed << setprecision(6) << tm << endl;
         }
     }
 
     file.close();
-
     return 0;
 }
 
